38b8c65d4380df7b9405dcadf511688bac24eee9
Taoyu Chen <875693150@qq.com>
2020-10-27 21:36:26 +0800

    Exercise 3 completed

--- stories/exercises.stories.js
+++ stories/exercises.stories.js
@@ -5,7 +5,7 @@ import CourseModulesEmbeddedVars from "../components/exercises/02_embeddedVariab
 import CourseModulesWithProps from "../components/exercises/03_props";
 import CourseModulesCollections from "../components/exercises/04_iteration";
 
-const course = "Software System Parcise";
+const name = "Software System Parcise - Modules table";
 const modules = [
   {
     name: "Enterprise Systems Architecture",
