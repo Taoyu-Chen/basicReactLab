ed3e110d0913618f46c7b93515c33c0776970211
Taoyu Chen <875693150@qq.com>
2020-10-27 15:20:10 +0800

    Exercise 1 completed

--- components/exercises/01_staticComponent.js
+++ components/exercises/01_staticComponent.js
@@ -3,8 +3,32 @@ import "../../node_modules/bootstrap/dist/css/bootstrap.css";
 
 const Demo = () => {
   return (
-      <h1>TODO</h1>
+    <div>
+      <h2>Software System Parcise - Modules table</h2>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>No lectures</th>
+            <th>No practicals</th>
+          </tr>
+        </thead>
+        <tbody >
+          <tr>
+            <td>Enterprise Systems Architecture</td>
+            <td>48</td>
+            <td>12</td>
+          </tr>
+          <tr>
+            <td>Business Analytics</td>
+            <td>24</td>
+            <td>36</td>
+          </tr>
+        </tbody >
+      </table>
+    </div>
   );
 };
+
 
 export default Demo;
